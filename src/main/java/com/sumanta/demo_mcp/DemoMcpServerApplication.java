ae49349798874a0ed9537fef289f01af83c31564
sumanta <sumantajashh@gmail.com>
2025-08-22 02:14:21 +0530

    basic project structure

---
+++ src/main/java/com/sumanta/demo_mcp/DemoMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.sumanta.demo_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumanta/demo_mcp/DemoMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumanta.demo_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
